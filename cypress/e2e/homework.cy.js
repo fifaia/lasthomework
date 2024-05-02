7763bc0388c4a837717c8353f80628fe61b98015
fifaia <36516921+fifaia@users.noreply.github.com>
2024-05-02 13:57:21 +0400

    change2

--- cypress/e2e/homework.cy.js
+++ cypress/e2e/homework.cy.js
@@ -1,5 +1,5 @@
 describe('zootopia.ge', () => {
-  it('Does not do much!', () => {
+  it('რეგისტრაცია ავტორიზაცია', () => {
     // რეგისტრაცია
     cy.viewport(1920, 1080);
     cy.visit('https://zootopia.ge/ka')
@@ -15,6 +15,187 @@ describe('zootopia.ge', () => {
     cy.get('.recovery > .input-shablon > .form-button').click().get('.input-shablon > .form-error > p').should('contain','არასწორი ელ_ფოსტა')
     //პაროლის აღდგენის გამორთვა
     cy.get('.recovery > .input-shablon > .xform').click()
+    //ჰედერში შესვლაზე კლიკი
+    cy.get('.iprof').click()
+    //რეგისტრაციაზე კლიკი
+    cy.get('.input-shablon > p > a').click()
+    // გადამოწმება, გადავიდა თუ არა რეგისტრაციის მისამართზე
+    cy.url().should('eq', 'https://zootopia.ge/ka/register');
+    // ავტორიზაციაზე კლიკი რეგისტრაციის გვერდზე -- ეს ქეისი არ მუშაობს ამიტომ დავაკომენტარებს რომ გააგრძელოს მუშაობა
+    // cy.get('.reeg > span').click().get('.avtorization > .input-shablon').should('be.visible')
+
+    // რეგისტრაცია სახელის გამოტოვებით
+
+    // მეილის ველის შევსება
+    cy.get(':nth-child(2) > .imail').type(`${rand.text(6)}@gmail.com`)
+    // პირადი ნომერის შევსება
+    cy.get('.ipir').type(`${rand.number(10000, 1000000)}`)
+    // ტელეფონის ნომრის შევსება
+    cy.get(':nth-child(4) > .itel').type(`${rand.number(100000, 10000000)}`)
+    //პაროლის შევსება
+    let pass = rand.text(5);
+    cy.get(':nth-child(5) > .ipass').type(pass)
+    cy.get('.reg-form-left > :nth-child(6) > .ipass').type(pass)
+    // წესები და პირობების თანხმობა
+    cy.get('#Path_10302').click()
+    // რეგისტრაციაზე კლიკი
+    cy.get('.regsub').click()
+    // სახელის სიცარიელის ერორი. შედეგად ვერ გაიარა რეგისტრაცია სახელის გარეშე
+    cy.get(':nth-child(1) > .alert > img').should('be.visible')
+
+    
+    // რეგისტრაცია მეილის გამოტოვებით
+
+    // სახელის ველის შევსება
+    cy.get(':nth-child(1) > .ismile').clear().type(`${rand.text(6)}`)
+    // მეილის ვერის გასუფთავება
+    cy.get(':nth-child(2) > .imail').clear()
+    // პირადი ნომერის შევსება
+    cy.get('.ipir').clear().type(`${rand.number(10000, 1000000)}`)
+    // ტელეფონის ნომრის შევსება
+    cy.get(':nth-child(4) > .itel').type(`${rand.number(100000, 10000000)}`)
+    //პაროლის შევსება
+    pass = rand.text(5);
+    cy.get(':nth-child(5) > .ipass').clear().type(pass)
+    cy.get('.reg-form-left > :nth-child(6) > .ipass').clear().type(pass)
+    // წესები და პირობების თანხმობა
+    cy.get('#Path_10302').click()
+    // რეგისტრაციაზე კლიკი
+    cy.get('.regsub').click()
+    // მეილის სიცარიელის ერორი. შედეგად ვერ გაიარა რეგისტრაცია მეილის გარეშე გარეშე
+    cy.get(':nth-child(2) > .alert > img').should('be.visible')
+
+
+    // რეგისტრაცია პირადი ნომრის გამოტოვებით
+
+    // სახელის ველის შევსება
+    cy.get(':nth-child(1) > .ismile').clear().type(`${rand.text(6)}`)
+    // მეილის ვერის გასუფთავება
+    cy.get(':nth-child(2) > .imail').clear().type(`${rand.text(6)}@gmail.com`)
+    // პირადი ნომერის გასუფთავება
+    cy.get('.ipir').clear()
+    // ტელეფონის ნომრის შევსება
+    cy.get(':nth-child(4) > .itel').type(`${rand.number(100000, 10000000)}`)
+    //პაროლის შევსება
+    pass = rand.text(5);
+    cy.get(':nth-child(5) > .ipass').clear().type(pass)
+    cy.get('.reg-form-left > :nth-child(6) > .ipass').clear().type(pass)
+    // წესები და პირობების თანხმობა
+    cy.get('#Path_10302').click()
+    // რეგისტრაციაზე კლიკი
+    cy.get('.regsub').click()
+    // პირადი ნომერის სიცარიელის ერორი. შედეგად ვერ გაიარა რეგისტრაცია პირადი ნომერის გარეშე
+    cy.get(':nth-child(3) > .alert > img').should('be.visible')
+
+    // რეგისტრაცია ტელეფონის ნომრის გამოტოვებით
+
+    // სახელის ველის შევსება
+    cy.get(':nth-child(1) > .ismile').clear().type(`${rand.text(6)}`)
+    // მეილის ვერის გასუფთავება
+    cy.get(':nth-child(2) > .imail').clear().type(`${rand.text(6)}@gmail.com`)
+    // პირადი ნომერის გასუფთავება
+    cy.get('.ipir').clear().type(`${rand.number(100000, 10000000)}`)
+    // ტელეფონის ნომრის გასუფთავება
+    cy.get(':nth-child(4) > .itel').clear()
+    //პაროლის შევსება
+    pass = rand.text(5);
+    cy.get(':nth-child(5) > .ipass').clear().type(pass)
+    cy.get('.reg-form-left > :nth-child(6) > .ipass').clear().type(pass)
+    // წესები და პირობების თანხმობა
+    cy.get('#Path_10302').click()
+    // რეგისტრაციაზე კლიკი
+    cy.get('.regsub').click()
+    // ტელეფონის ნომრის სიცარიელის ერორი. შედეგად ვერ გაიარა რეგისტრაცია ტელეფონის ნომრის გარეშე
+    cy.get('.reg-form-left > :nth-child(4) > .alert > img').should('be.visible')
+
+    // რეგისტრაცია პაროლის გამოტოვებით
+
+    // სახელის ველის შევსება
+    cy.get(':nth-child(1) > .ismile').clear().type(`${rand.text(6)}`)
+    // მეილის ვერის შევსება
+    cy.get(':nth-child(2) > .imail').clear().type(`${rand.text(6)}@gmail.com`)
+    // პირადი ნომერის შევსება
+    cy.get('.ipir').clear().type(`${rand.number(100000, 10000000)}`)
+    // ტელეფონის ნომრის შევსება
+    cy.get(':nth-child(4) > .itel').clear().type(`${rand.number(100000, 10000000)}`)
+    //პაროლის გასუფთაება
+    pass = rand.text(5);
+    cy.get(':nth-child(5) > .ipass').clear()
+    cy.get('.reg-form-left > :nth-child(6) > .ipass').clear()
+    // წესები და პირობების თანხმობა
+    cy.get('#Path_10302').click()
+    // რეგისტრაციაზე კლიკი
+    cy.get('.regsub').click()
+    // პაროლის სიცარიელის ერორი. შედეგად ვერ გაიარა რეგისტრაცია პაროლის გარეშე
+    cy.get('.input-div.alert > .alert > img').should('be.visible')
+
+    // რეგისტრაცია წესებზე და პირობებზე თანხმობის გამოტოვებით
+
+    // სახელის ველის შევსება
+    cy.get(':nth-child(1) > .ismile').clear().type(`${rand.text(6)}`)
+    // მეილის ვერის შევსება
+    cy.get(':nth-child(2) > .imail').clear().type(`${rand.text(6)}@gmail.com`)
+    // პირადი ნომერის შევსება
+    cy.get('.ipir').clear().type(`${rand.number(100000, 10000000)}`)
+    // ტელეფონის ნომრის შევსება
+    cy.get(':nth-child(4) > .itel').clear().type(`${rand.number(100000, 10000000)}`)
+    //პაროლის შევსება
+    pass = rand.text(5);
+    cy.get(':nth-child(5) > .ipass').clear().type(pass)
+    cy.get('.reg-form-left > :nth-child(6) > .ipass').clear().type(pass)
+    // წესები და პირობების თანხმობა
+    // cy.get('#Path_10302').click()
+    // რეგისტრაციაზე კლიკი
+    cy.get('.regsub').click()
+    // წესებზე და პირობებზე თანხმობის ერორი. შედეგად ვერ გაიარა რეგისტრაცია წესებზე და პირობებზე თანხმობის გარეშე
+    cy.get('.etx').should('have.class', 'err')
+
+
+    // icon ის არჩევა
+    cy.get('[for="profile2"]').click()
+  })
+
+  it('კალათა', () => {
+    cy.viewport(1920, 1080);
+    cy.visit('https://zootopia.ge/ka')
+    //კალათაში ნივთების დამატება
+    cy.get('.product-cart').eq(1).click()
+    //გადამოწმება რომ დაემატა
+    cy.get('.product-cart').eq(1).should('have.class', 'add')
+
+    cy.get('.product-cart').eq(2).click()
+    //გადამოწმება რომ დაემატა
+    cy.get('.product-cart').eq(2).should('have.class', 'add')
+
+    cy.get('.product-cart').eq(3).click()
+    //გადამოწმება რომ დაემატა
+    cy.get('.product-cart').eq(3).should('have.class', 'add')
+
+    //რაოდენობის გადამოწმება
+    cy.get('.icart > #cart-items-count').should('have.text', '3');
+
+    //გადასვლა კალათის გვერდზე
+    cy.get('.icart').click()
+
+    //მისამართის გადამოწმება
+    cy.url().should('eq', 'https://zootopia.ge/ka/cart');
+    //+ ზე კლიკი
+    cy.get(':nth-child(1) > .spinner > .plus').click()
+    //გადამოწმება თუ მოემატა პროდუქტის რაოდენობა
+    cy.get(':nth-child(1) > .spinner > input').should('have.value', '2');
+    //მინუსზე დაჭერა
+    cy.get(':nth-child(1) > .spinner > .minus').click();
+    //გადამოწმება თუ დააკლდა პროდუქტის რაოდენობა
+    cy.get(':nth-child(1) > .spinner > input').should('have.value', '1');
+    //პროდუქტის წაშლის იკონკა
+    cy.get('.cart-list > :nth-child(1)').should('be.visible')
+    //პროდუქციის წაშლა
+    cy.get('.cart-list > :nth-child(1)').click()
+
+    cy.get('.icart').click()
+    //რაოდენობის გადამოწმება
+    cy.get('.cart-item > .clear').eq(1).click();
+
   })
 })
 
